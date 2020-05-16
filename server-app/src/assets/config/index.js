b7a40b803878d16d4046eb8bafeecc589e59d7d3
Teruki Goto <teruki.gotoh@gmall.com>
2020-05-17 02:14:18 +0900

    update heroku build script

--- server-app/src/assets/config/index.js
+++ server-app/src/assets/config/index.js
@@ -1,8 +1,8 @@
 if (process.env.NODE_ENV === 'production') {
   module.exports = require('./config.prod')
 }
-if (process.env.NODE_ENV === 'prod-local') {
-  module.exports = require('./config.prod-local')
-} else {
-  module.exports = require('./config.dev')
-}
+// if (process.env.NODE_ENV === 'prod-local') {
+//   module.exports = require('./config.prod-local')
+// } else {
+//   module.exports = require('./config.dev')
+// }
