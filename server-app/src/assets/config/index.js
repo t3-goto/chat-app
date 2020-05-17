9b4c755dc4f62e61dc0b11a8b9b8b7a7de1e6780
Teruki Goto <teruki.gotoh@gmall.com>
2020-05-17 14:19:48 +0900

    update config settings

--- server-app/src/assets/config/index.js
+++ server-app/src/assets/config/index.js
@@ -1,8 +1,3 @@
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./config.prod')
-}
-// if (process.env.NODE_ENV === 'prod-local') {
-//   module.exports = require('./config.prod-local')
-// } else {
-//   module.exports = require('./config.dev')
-// }
+configFilename = './config.' + process.env.NODE_ENV + '.js'
+console.log(configFilename)
+module.exports = require(configFilename)
